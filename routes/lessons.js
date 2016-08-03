769e9cd19197430468df8a1b8f9043fb765ab651
Chris Lusto <ctlusto@gmail.com>
2016-08-03 17:31:28 -0400

    Basic view for new lesson

--- routes/lessons.js
+++ routes/lessons.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('lessons/index');
 });
 
+// Show the UI for creating a new lesson
+router.get('/new', function(req, res, next) {
+  res.render('lessons/new');
+});
+
 module.exports = router;
