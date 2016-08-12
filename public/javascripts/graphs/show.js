aabbf795867bd3365284a5b41b9552c6988b340f
Chris Lusto <ctlusto@gmail.com>
2016-08-12 14:41:31 -0400

    Move fetching graph data to the frontend code to prevent unescaped JSON in the view

--- public/javascripts/graphs/show.js
+++ public/javascripts/graphs/show.js
@@ -1,10 +1,23 @@
 $(function() {
   
-  var elt = $('#calculator')[0];
-  var options = JSON.parse(graphData.options); // the saved options from the author
-  var calc = Desmos.Calculator(elt, options); // instantiate a calculator with those options
-  $('.progress').remove();
+  function getGraphID() {
+    var fullPath = location.pathname;
+    id = fullPath.substr(fullPath.lastIndexOf('/') + 1);
+    return id;
+  }
+  var id = getGraphID();
   
-  calc.setState(graphData.state); // set the state of the current calculator to the saved state
+  var elt = $('#calculator')[0];
+
+  // Fetch the graph data from the db
+  $.get('/graphs/api/' + id)
+    .done(function(data) {
+      var options = JSON.parse(data.options); // the saved options from the author
+      var calc = Desmos.Calculator(elt, options); // instantiate a calculator with those options
+      $('.progress').remove();
+      
+      calc.setState(data.state); // set the state of the current calculator to the saved state
+      $('.btn-floating').attr('href', '/graphs/edit/' + id); // Hook up the edit button to the correct route
+    });
   
 });
